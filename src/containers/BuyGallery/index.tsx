d09388df006ab8fde939c629b206af3be20a73ef
Michael Wu <xu.michael.wu@gmail.com>
2022-07-04 17:22:49 +1000

    move hooks

--- src/containers/BuyGallery/index.tsx
+++ src/containers/BuyGallery/index.tsx
@@ -1,16 +1,17 @@
 import Box from 'components/common/Box'
+import Flex from 'components/common/Flex'
 import Text from 'components/common/Text'
 import withSuspense from 'hooks/withSuspense'
 import React from 'react'
 
 const BuyGallery = withSuspense(() => {
   return (
-    <>
+    <Flex>
       <Box>
         <Text variant="heading">My Puts</Text>
         <Text variant="heading">My NFTs</Text>
       </Box>
-    </>
+    </Flex>
   )
 })
 
