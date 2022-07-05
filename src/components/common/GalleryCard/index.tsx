b9d3b99ae9c8dc4181616e77e5a4110aed72c059
Michael Wu <xu.michael.wu@gmail.com>
2022-07-05 13:43:50 +1000

    GalleryCard hover state

--- src/components/common/GalleryCard/index.tsx
+++ src/components/common/GalleryCard/index.tsx
@@ -1,4 +1,5 @@
 import { GALLERY_CARD_WIDTH } from 'containers/BuyGallery'
+import useThemeColor from 'hooks/ui/useThemeColor'
 import { PutData } from 'hooks/useBids'
 import { NFTData } from 'hooks/useMyNFTs'
 import React from 'react'
@@ -7,24 +8,53 @@ import { MarginProps } from 'styled-system'
 import Card from '../Card'
 import CardBody from '../Card/CardBody'
 import Flex from '../Flex'
+import Icon, { IconType } from '../Icon'
 import Image from '../Image'
 import Text from '../Text'
 
 type Props = {
   nftData: NFTData
   option?: PutData
+  isSelected?: boolean
+  handleClick?: (nftData: NFTData) => void
 } & MarginProps
 
-export default function GalleryCard({ nftData, option }: Props): JSX.Element {
+export default function GalleryCard({ nftData, option, handleClick, isSelected = false }: Props): JSX.Element {
+  const background = useThemeColor('background')
   return (
     <Card
       minWidth={GALLERY_CARD_WIDTH}
       onClick={() => {
-        console.log(nftData)
+        if (handleClick) {
+          handleClick(nftData)
+        }
+      }}
+      sx={{
+        ':hover': {
+          bg: 'buttonHover',
+          cursor: 'pointer',
+        },
       }}
     >
       <CardBody>
-        <Flex width="100%" justifyContent="center">
+        <Flex width="100%" justifyContent="center" sx={{ position: 'relative' }}>
+          <Flex
+            justifyContent="center"
+            alignItems="center"
+            width="100%"
+            height="100%"
+            sx={{
+              position: 'absolute',
+              top: 0,
+              left: 0,
+              background: background + '80',
+              transition: 'opacity 0.15s ease-out',
+              backdropFilter: 'blur(4px)',
+              opacity: isSelected ? 1 : 0,
+            }}
+          >
+            <Icon color="primary" icon={IconType.Check} size={80} />
+          </Flex>
           <Image src={nftData.image_url_copy} size={180} />
         </Flex>
         <Text my={3} variant="bodyMedium">
